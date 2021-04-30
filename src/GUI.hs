b1c73dc007394783f73f96b88a3382ceae5441c0
PedroDCN <pedroxcg@gmail.com>
2021-04-29 21:17:21 -0300

    Correção da GUI e adição de comentários

--- src/GUI.hs
+++ src/GUI.hs
@@ -5,26 +5,23 @@ import Graphics.UI.Threepenny.Core
 import Data.List
 import Estilo
 
-config :: Window -> UI()
+config :: Window -> UI ()
 config window = do
-    return window # set UI.title "Tabuleiro"
+    return window # set UI.title "Xadrez 2D" -- define título da página
 
-    -- definição tabuleio e notações
+    -- definição tabuleio
     tab <- UI.div #. "tabuleiro"
         # set style cssBoard
 
-    -- div com tabuleiro e notações
+    -- div container do tabuleiro
     divtab <- UI.div #. "tab"
         #+ [element tab]
-        # set style [("top", "50%"), ("left", "30%"), ("width", "50%"), 
-        ("position", "absolute"), ("transform", "translateY(-50%)"), 
-        ("-webkit-transform", "translateY(-50%)"), ("-moz-transform", "translateY(-50%)")]
-        -- #+ [element tab, element divntc2, element divntc1]
+        # set style cssContainerBoard
 
-    -- adiciona casas no tabuleiro
-    adicionacasas (element tab) Branca
+    -- adiciona as casas no tabuleiro - malha quadriculada 8x8
+    adicionaCasas (element tab) Branca
 
-    -- definição textos invisíveis para guardar jogadas
+    -- definição textos invisíveis para guardar jogada
     j1 <- UI.string "" #. "j1" # set UI.id_ "j1"
     j2 <- UI.string "" #. "j2" # set UI.id_ "j2"
     divinvisivel <- UI.div #. "mov" -- div invisível que guarda as casas da jogada atual
@@ -33,91 +30,67 @@ config window = do
 
     -- adiciona div do tabuleiro e dos botões em div principal
     principal <- UI.div #. "principal"
-        -- # set style [("width", "50%"), ("margin", "auto"), ("border", "2px solid blue"), ("padding", "5px")]
         #+ [element divtab, element divinvisivel]
-        # set style [("position", "absolute"), ("width", "100%"), ("height", "100%"), 
-        ("background-color","#ccc")]
-        -- # set style [("padding", "5px"), ("margin", "0"), ("position", "absolute"), ("top", "50%"), ("left", "50%"), ("transform", "translate(-50%,-50%)"), ("-webkit-transform", "translate(50%, 50%)")]        
-        -- #+ [element divtab, element divbtns]
-
-    
+        # set style cssContainerPrincipal
 
     -- adiciona div principal no body
     getBody window #+ [element principal]
 
     -- adicionando peças na posição inicial
-    adicionapecas
+    adicionaPecas
 
-    fazHoverPecas -- define funções de efeito de hover in e out de todas as casas do tabuleiro
+    -- define funções de efeito de hover in e out de todas as casas do tabuleiro
 
-    fazMovimentoCasas -- define funções para movimento de peças ao clicar para qualquer casa do tabuleiro
+    fazHoverPecas
+    -- define funções para movimento de peças ao clicar para qualquer casa do tabuleiro
+    fazMovimentoCasas
 
--- ================================================================================================================================================    
-
-
+-- função para fazer jogada dada o clique de uma peça
+addJogada :: String -> UI ()
 addJogada f = do
     w <- askWindow
     j1 <- getElementsByClassName w "j1"
     j2 <- getElementsByClassName w "j2"
     let pos1 = head j1
     let pos2 = head j2
-
-    -- log para debug
     text1 <- callFunction $ pegaTexto "j1"
     text2 <- callFunction $ pegaTexto "j2"
-    -- consoleLog ("antes da jogada: " ++ text1)
-    -- consoleLog ("antes da jogada: " ++ text2)
-
     if text1 == f then return () else do
         if null text1 then do
             element pos1 # set UI.text f
-
             element pos1 # set style cssHighlight
-
-            -- log para debug
-            -- t1 <- callFunction $ pegaTexto "j1"
-            -- consoleLog ("depois da jogada: " ++ t1)
-
             return ()
         else do
             if null text2 then do
                 element pos2 # set UI.text f
-
-                -- log para debug
-                -- t2 <- callFunction $ pegaTexto "j2"
-                -- consoleLog ("depois da jogada: " ++ t2)
-
                 movePeca text1 f
                 element pos1 # set UI.text ""
                 element pos2 # set UI.text ""
-
                 callFunction $ removeSombraCasa text1
-
                 return ()
             else do
                 return ()
 
-
-adicionacasas widg cor = do
-    -- w <- askWindow
-    add 64 widg cor
+-- Função que adiciona as casas do tabuleiro, seguindo o padrão quadriculado do jogo
+adicionaCasas :: UI Element -> Cor -> UI ()
+adicionaCasas widg cor = do
+    add' 64 widg cor
     return ()
 
-add :: Int -> UI Element -> Cor -> UI ()
-add 0 w cor = return ()
-add n w cor = do
-        -- t <- UI.string ("div " ++ show (64 - n + 1) ++ " " ++ listaCasas !! (64 - n) ++ " ")      -- texto padrão para colocar na casa para reconhecimento e testes
+-- função auxiliar de adicionaCasas
+add' :: Int -> UI Element -> Cor -> UI ()
+add' 0 w cor = return ()
+add' n w cor = do
         d <- UI.div #. ((listaCasas !! (64 - n)) ++ " " ++ (if cor == Branca then white else black))
             # set style (cssSquare ++ (if cor == Branca then cssWhite else cssBlack))
             # set UI.id_ (listaCasas !! (64 - n))
-            -- #+ [element t]
         w #+ [element d]
-        -- runFunction (ffi ("console.log(\'" ++ "div adicionada: " ++ show (64 - n + 1) ++ " " ++ listaCasas !! (64 - n) ++ "  cor do quadrado: " ++ show cor ++ "\')")::JSFunction ())
-        add (n-1) w (if (64 - n + 1) `mod` 8 == 0 then cor else troca cor)
+        add' (n-1) w (if (64 - n + 1) `mod` 8 == 0 then cor else troca cor)
 
 
--- adiciona peças na posição inical no tabulerio
-adicionapecas = do
+-- função que adiciona as peças na posição inicial do jogo
+-- TODO: fazer mais funcional -> lista de pares casa peça e passar num foldl
+adicionaPecas = do
     -- w <- askWindow
     -- adiciona peças brancas
     addPeca "a1" "wr"
@@ -156,22 +129,29 @@ adicionapecas = do
     addPeca "g7" "bp"
     addPeca "h7" "bp"
 
-loadPeca :: String -> UI Element
-loadPeca peca = do
+-- função auxiliar para carregar na janela a imagem de uma peça
+loadPeca' :: String -> UI Element
+loadPeca' peca = do
     url <- loadFile "image/png" ("src/pb/" ++ peca ++".png")
     img <- mkElement "img" # set UI.width 90 # set UI.src url
     mkElement "div" #. ("peca-" ++ peca) #+ [element img]
 
+-- função que adiciona uma peça na casa especificada
 addPeca :: String -> String -> UI ()
 addPeca casa peca = do
     window <- askWindow
     c <- UI.getElementsByClassName window casa
-    cp <- loadPeca peca
+    cp <- loadPeca' peca
     element (head c) # set children [] -- limpo o children pra ficar vazio e colocar peça nova
     element (head c) #+ [element cp]
     return ()
 
-movePeca casai casaf = do -- usar função addPeca para a casa final - addPeca casai tipoPeca - reaproveitar
+-- função que move uma peça da casaInicial (ex: a2) até a casaFinal especificada.
+-- Caso a casa inicial seja vazia, não realiza nenhum movimento.
+-- TODO: usar a função addPeca para posicionar peça na casa final
+-- TODO: Reaproveitar img da casaInicial (usar ffi), sem precisar criar img nova
+-- TODO: adicionar peça removida (caso exista) na lista de peças capturadas
+movePeca casai casaf = do
     if casai == "" then return () else do
         temPeca <- verificaTemPecaCasa casai
         if temPeca then do
@@ -183,75 +163,35 @@ movePeca casai casaf = do -- usar função addPeca para a casa final - addPeca c
             let casaIni = head ci
             let casaFin = head cf
             let tipoPeca = drop 5 valorCasaini
-            -- consoleLog tipoPeca
-            cp <- loadPeca tipoPeca -- cria div com imagem da peça da casa inicial
-            element casaIni # set children [] -- limpa casa de inicio
-            element casaFin # set children [] -- limpa casa de final para colocar peça
-            -- TODO: adicionar peça removida (caso exista) na lista de peças capturadas
+            cp <- loadPeca' tipoPeca -- cria div com imagem da peça da casa inicial
+            element casaIni # set children [] -- limpa casa de início
+            element casaFin # set children [] -- limpa casa final para colocar peça
             element casaFin #+ [element cp]
             return ()
         else do return ()
 
-consoleLog' :: String -> JSFunction ()
-consoleLog' = ffi "console.log(%1)"
-
-consoleLog :: String -> UI ()
-consoleLog = runFunction . consoleLog'
-
-consoleL :: String -> UI()
-consoleL = runFunction . ffi "console.log(%1)"
-
-pegaTexto :: String -> JSFunction String
-pegaTexto = ffi "document.getElementsByClassName(%1).item(0).innerHTML"
-
-pegaCasa :: String -> JSFunction String -- pega o class da casa "c1" -> div c1
-pegaCasa = ffi "document.getElementsByClassName(%1).item(0).classList"
-
-pegaEstiloCasa :: String -> JSFunction String
-pegaEstiloCasa = ffi "document.getElementsByClassName(%1).item(0).attributes.getNamedItem(\'style\').value"
-
-pegaTipoPecaCasa :: String -> JSFunction String
-pegaTipoPecaCasa = ffi "document.getElementsByClassName(%1).item(0).firstElementChild.attributes.getNamedItem(\'class\').value"
-
-pegaClasseCasa :: String -> JSFunction String
-pegaClasseCasa = ffi "document.getElementsByClassName(%1).item(0).attributes.getNamedItem(\'class\').value"
-
-depPegaTipo :: String -> UI ()
-depPegaTipo casa = do
-    txt <- callFunction $ pegaTipoPecaCasa casa
-    consoleLog txt
-
+-- função que verifica se dada uma casa (ex: a2), existe uma peça nela, retorna um UI Bool.
 verificaTemPecaCasa :: String -> UI Bool
 verificaTemPecaCasa casa = do
     t <- callFunction $ pegaTexto casa
     if t == "" then return False else return True
 
-pegaEstiloSombraCasa :: String -> JSFunction String
-pegaEstiloSombraCasa = ffi "window.getComputedStyle(document.getElementsByClassName(%1).item(0)).getPropertyValue(\"box-shadow\")"
-
-removeSombraCasa :: String -> JSFunction ()
-removeSombraCasa = ffi "document.getElementsByClassName(%1).item(0).style.removeProperty(\'box-shadow\')"
-
+-- função que retorna o UI Element para um dado valor de class de uma casa
 getUIElementCasa :: String -> UI Element
 getUIElementCasa casa = do
     window <- askWindow
     elemento <- UI.getElementsByClassName window casa
     return (head elemento)
 
-limpaConteudo :: String -> JSFunction ()
-limpaConteudo = ffi "document.getElementsByClassName(%1).item(0).innerHTML = vara6"
-
-data Cor = Sem | Preta | Branca deriving (Eq, Ord, Show, Read, Bounded, Enum)
-
-troca Branca = Preta
-troca Preta = Branca
-
+-- função que cria os eventos de hover das casas,
+-- dando highlight nas casas ao passar o mouse por cima delas
 fazHoverPecas :: UI ()
 fazHoverPecas = do
-    mapM_ fazHoverPeca listaCasas 
+    mapM_ fazHoverPeca' listaCasas 
 
-fazHoverPeca :: String -> UI ()
-fazHoverPeca x = do
+-- função auxiliar que cria o evento de hover para uma casa dada
+fazHoverPeca' :: String -> UI ()
+fazHoverPeca' x = do
     px <- getUIElementCasa x
 
     on UI.hover px $ const $ do
@@ -262,13 +202,45 @@ fazHoverPeca x = do
         if text == x then return () else do
             callFunction $ removeSombraCasa x
 
+-- função que cria os eventos de mover uma peça no tabuleiro por meio de cliques nas casas
 fazMovimentoCasas :: UI ()
 fazMovimentoCasas = do
-    mapM_ fazMovimentoPeca listaCasas
+    mapM_ fazMovimentoPeca' listaCasas
 
-fazMovimentoPeca :: String -> UI ()
-fazMovimentoPeca x = do     -- função genérica adiciona bind de click para movimento de peça na casa 'x'
+-- função auxiliar que cria o evento de movimento para uma casa dada
+fazMovimentoPeca' :: String -> UI ()
+fazMovimentoPeca' x = do  -- função genérica adiciona bind de click para movimento de peça na casa 'x'
     px <- getUIElementCasa x
 
     on UI.click px $ const $ do
         addJogada x
+
+-- Tipo de cor das casas do tabuleiro, usado para auxiliar na criação do tabuleiro
+data Cor = Sem | Preta | Branca deriving (Eq)
+
+-- função para trocar o valor de um elemento do tipo Cor
+troca :: Cor -> Cor
+troca Branca = Preta
+troca Preta = Branca
+
+-- Funções utilitárias de outras funções
+
+-- função utilitária que pega o valor de sombra de uma dada casa
+pegaEstiloSombraCasa :: String -> JSFunction String
+pegaEstiloSombraCasa = ffi "window.getComputedStyle(document.getElementsByClassName(%1).item(0)).getPropertyValue(\"box-shadow\")"
+
+-- função utilitária que remove o highlight de uma dada casa
+removeSombraCasa :: String -> JSFunction ()
+removeSombraCasa = ffi "document.getElementsByClassName(%1).item(0).style.removeProperty(\'box-shadow\')"
+
+-- função utilitária que pega o tipo da peça para uma casa (ex: a2) dada
+pegaTipoPecaCasa :: String -> JSFunction String
+pegaTipoPecaCasa = ffi "document.getElementsByClassName(%1).item(0).firstElementChild.attributes.getNamedItem(\'class\').value"
+
+-- função utilitária para pegar texto interno de um elemento dado da tela
+pegaTexto :: String -> JSFunction String
+pegaTexto = ffi "document.getElementsByClassName(%1).item(0).innerHTML"
+
+-- função utilitária para debug
+-- consoleLog' :: String -> UI()
+-- consoleLog' = runFunction . ffi "console.log(%1)"
